34237df97ec8f7a5e5f82ea8bc24908ca8b6d29c
F180243 <f180243@nu.edu.pk>
2022-06-06 03:34:08 +0500

    splash page complete

---
+++ frontend/src/pages/Splash.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import logo from "../assets/logo.jpeg";
+import styled from "styled-components";
+
+
+export default function Splash() {
+  return (
+      
+    <Container>
+      <LogoWrapper>
+        <img src={logo}/>
+
+        <h3>Student Affair Management System</h3>
+
+        <Button ><i class="fa fa-arrow-right"></i></Button>
+
+
+      </LogoWrapper>
+
+    </Container>
+  )
+}
+
+const Container = styled.div`
+display: flex;
+align-items: center;
+  
+  `
+
+  const LogoWrapper = styled.div`
+  img {
+   
+  width: 20%;
+  margin-top:6rem;
+  margin-left: auto;
+  margin-right:auto;
+  display: block;
+  
+  }
+  h3 {
+    text-align:center;
+    color: #000000;
+    margin-left: auto;
+    margin-right:auto;
+    width:18%;
+    font-size: 22px;
+  }
+  
+`;
+
+
+const Button = styled.div`
+margin-left: auto;
+margin-right:auto; 
+width:15%;
+background-color: #174A84; 
+border: none; 
+color: white; /* White text */
+padding: 12px 16px; 
+cursor: pointer; 
+&:hover {
+  background-color: #174A80;
+}
+i {
+  text-align:center;
+  display: block;
+  margin-left: auto;
+  margin-right:auto;
+}
+
+`
